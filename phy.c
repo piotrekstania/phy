fce4eda58a7fe90c8a2eaed9ddfa17dc3f2363be
piotrekstania <piotrekstania@gmail.com>
2016-06-04 10:08:10 +0200

    new functions

--- phy.c
+++ phy.c
@@ -1,1 +1,37 @@
 #include "phy.h"
+#include <math.c>
+
+/*
+  Funkcja do obliczania wilgotnosci bezwzglednej z podanej temperatury
+  i wilgotnosci wzglednej. Wartosci z poza zakresu beda przycinane
+  do wartosci granicznych.
+  
+  arg 1: (double) temperatura (-20..50 °C)
+  arg 2: (double) wilgotnosc wzgledna (0..100 %)
+  ret  : (double) wilgotnosc bezwzgledna (g/m³)
+*/
+
+double phyCalcAh(double temp, double rh) {
+  
+  if(temp < -20.0) temp = -20.0;
+  else if(temp > 50.0) temp = 50.0;
+  
+  if(rh < 0.0) rh = 0.0;
+  else if(rh > 100.0) rh = 100.0;
+  
+  return (double)(216.7*(((rh/100)*6.112*exp((17.62*temp)/(243.12+temp)))/(273.15+temp)));
+}
+
+/*
+  Funkcja do obliczania punktu rosy z podanej temperatury i wilgotnosci
+  wzglednej. Wartosci z poza zakresu beda przyciete do wartosci
+  granicznych.
+  
+  arg 1: (double) temperatura (-20..50 °C)
+  arg 2: (double) wilgotnosc wzgledna (0..100 %)
+  ret  : (double) punkt rosy (°C)
+*/
+
+double phyCalcDp(double temp, double rh) {
+  
+}
--- phy.h
+++ phy.h
@@ -3,10 +3,26 @@
 
 /*
   Funkcja do obliczania wilgotnosci bezwzglednej z podanej temperatury
-  i wilgotnosci wzglednej.
-  arg 1: (float) temperatura stC
-  arg 2: (float) wilgotnosc wzgledna %
-  ret  : (float) wilgotnosc bezwzgledna g/m3
+  i wilgotnosci wzglednej. Wartosci z poza zakresu beda przycinane
+  do wartosci granicznych.
+  
+  arg 1: (double) temperatura (-20..50 °C)
+  arg 2: (double) wilgotnosc wzgledna (0..100 %)
+  ret  : (double) wilgotnosc bezwzgledna (g/m³)
 */
+
+double phyCalcAh(double temp, double rh);
+
+/*
+  Funkcja do obliczania punktu rosy z podanej temperatury i wilgotnosci
+  wzglednej. Wartosci z poza zakresu beda przyciete do wartosci
+  granicznych.
+  
+  arg 1: (double) temperatura (-20..50 °C)
+  arg 2: (double) wilgotnosc wzgledna (0..100 %)
+  ret  : (double) punkt rosy (°C)
+*/
+
+double phyCalcDp(double temp, double rh);
 
 #endif //_phy_h
