440cbe615e3e6a859827f72697e5b5abbb8a0a20
piotrekstania <piotrekstania@gmail.com>
2016-06-04 09:41:21 +0200

    create main files

---
+++ phy.h
@@ -0,0 +1,12 @@
+#ifndef _phy_h
+#define _phy_h
+
+/*
+  Funkcja do obliczania wilgotnosci bezwzglednej z podanej temperatury
+  i wilgotnosci wzglednej.
+  arg 1: (float) temperatura stC
+  arg 2: (float) wilgotnosc wzgledna %
+  ret  : (float) wilgotnosc bezwzgledna g/m3
+*/
+
+#endif //_phy_h
